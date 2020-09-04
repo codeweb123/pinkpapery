9bb727988cc95e1b45ae0afa4af039c9dbf68ace
codeweb123 <jiyonj@hotmail.com>
2020-09-03 21:37:54 -0400

    add signin form and event handler method

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.jsx';
 import Navbar from './components/navbar/navbar.jsx';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp} />
       </Switch>
     </div>
   );
