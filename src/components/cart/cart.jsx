839f177f4de37529e3c2a80d4b1c3af968f7d430
codeweb123 <jiyonj@hotmail.com>
2020-09-08 17:15:26 -0400

    create cart items

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Button from '../button/button';
+import CartItem from '../cart-item/cart-item.jsx'
 import './cart.styles.scss';
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
     <div className= 'cart'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => ( 
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
+            </div>
         <Button>Go To Checkout</Button>
     </div>
-)
+);
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(Cart);
 
